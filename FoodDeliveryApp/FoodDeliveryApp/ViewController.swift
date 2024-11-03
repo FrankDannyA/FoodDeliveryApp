4e5cc562d8043d52558d5bfeec69ca0caaf2543c
Frank <orgfrank@icloud.com>
2024-11-03 14:07:37 +0500

    added new fonts

--- FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -12,6 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        
+        let label = UILabel()
+        label.text = "Привет Kolesa"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20),
+        ])
+        
+        label.font = .Roboto.bold.size(of: 40)
+        label.textColor = .black
     }
 }
 
+ 
