00070fb8a705465cce140b5333e661cca5f6de2a
Frank <orgfrank@icloud.com>
2024-11-03 13:13:34 +0500

    staged changes

--- FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -1,19 +1,17 @@
-//
-//  ViewController.swift
-//  FoodDeliveryApp
-//
-//  Created by Даниил Франк on 03.11.2024.
-//
+    //
+    //  ViewController.swift
+    //  FoodDeliveryApp
+    //
+    //  Created by Даниил Франк on 03.11.2024.
+    //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
-
-
 }
 
